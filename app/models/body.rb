da039166de39311e6288a1284e61038214b70e0d
Benjamin Marquis <bmarquiscom@gmail.com>
2019-11-01 10:57:24 -0400

    implemented add patient

--- app/models/body.rb
+++ app/models/body.rb
@@ -4,7 +4,7 @@ class Body < ApplicationRecord
 
   after_create do
     if muscles.empty?
-      new_muscles = [{name: "SCM", force_right: rand(0..4)}, {name: "Trap", force_right: rand(0..4)}, {name: "Lat", force_right: rand(0..4)}, {name: "Pec", force_right: rand(0..4)}, {name: "Deltoide", force_right: rand(0..4)}, {name: "Biceps", force_right: rand(0..4)}, {name: "Triceps", force_right: rand(0..4)}, {name: "PT", force_right: rand(0..4)}, {name: "Supi", force_right: rand(0..4)}, {name: "ECRL(B)", force_right: rand(0..4)}, {name: "FCR", force_right: rand(0..4)}, {name: "FDP", force_right: rand(0..4)}, {name: "EDC", force_right: rand(0..4)}, {name: "FDI", force_right: rand(0..4)}, {name: "APB", force_right: rand(0..4)}, {name: "Psoas", force_right: rand(0..4)}, {name: "Add long", force_right: rand(0..4)}, {name: "Quad", force_right: rand(0..4)}, {name: "Tib ant", force_right: rand(0..4)}, {name: "PL", force_right: rand(0..4)}, {name: "Tib post", force_right: rand(0..4)}, {name: "Ischios", force_right: rand(0..4)}, {name: "Glut med", force_right: rand(0..4)}, {name: "Gastrocs", force_right: rand(0..4)}, {name: "Glut max", force_right: rand(0..4)} ]
+      new_muscles = [{name: "SCM"}, {name: "Trap"}, {name: "Lat"}, {name: "Pec"}, {name: "Deltoide"}, {name: "Biceps"}, {name: "Triceps"}, {name: "PT"}, {name: "Supi"}, {name: "ECRL(B)"}, {name: "FCR"}, {name: "FDP"}, {name: "EDC"}, {name: "FDI"}, {name: "APB"}, {name: "Psoas"}, {name: "Add long"}, {name: "Quad"}, {name: "Tib ant"}, {name: "PL"}, {name: "Tib post"}, {name: "Ischios"}, {name: "Glut med"}, {name: "Gastrocs"}, {name: "Glut max"} ]
       new_muscles.each do |muscle|
         muscles << Muscle.new(muscle)
       end
