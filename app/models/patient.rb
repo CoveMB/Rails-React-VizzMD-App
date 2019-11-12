4836d001351baf04dfe3b170122d711bef6e9410
Benjamin Marquis <bmarquiscom@gmail.com>
2019-11-11 19:06:45 -0500

    before save with date to avoid app crash

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -7,4 +7,13 @@ class Patient < ApplicationRecord
   validates :date_of_birth, presence: true
 
   validates :gender, inclusion: { in: ["male", "female"] }
+
+  before_save do
+    if date_of_birth.nil?
+      date_of_birth = Date.new(2001,2,3)
+    end
+    if gender.nil?
+      gender = ["male", "female"].sample
+    end
+  end
 end
